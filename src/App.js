db68f9de0e44b55d3c86ed7ed66a2d057fef4588
ArnodDadayan <dadayanaa@yahoo.com>
2019-11-04 23:39:04 +0400

    hw setup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import React from 'react';
-import Header from './components/header/header'
-import Card from './components/card/card';
-
-import { data } from './data';
 
 const App = () => {
   return (
     <div className='container'>
-      <Header />
-      {data.map(item => {
-        const { author, content, description, title, id } = item;
-        return <Card key={id} author={author} content={content} description={description} title={title}/>
-      })}
+      {/* Code here */}
     </div>
   )
 }
